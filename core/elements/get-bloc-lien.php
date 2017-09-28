3a679b4c49188117fe814672012f792c8aee0294
Rolland Yann <rollandyann@gmail.com>
2017-09-28 13:03:59 +0200

    options links

--- core/elements/get-bloc-lien.php
+++ core/elements/get-bloc-lien.php
@@ -4,8 +4,8 @@
 	 * get_bloc_lien
 	 *
 	 *  @type	function
-	 *  @date	23/01/17
-	 *  @since	1.0.0
+	 *  @date	28/09/17
+	 *  @since	1.1.0
 	 *
 	 *  @param INT $post_id
 	 *  @return HTML - ACF text fields
@@ -13,14 +13,21 @@
 
 	function get_bloc_lien (){
 
-		$fluxi_content_lien = get_sub_field('texte_lien');		
+		$fluxi_content_lien = get_sub_field('texte_lien');
+		$type_lien = get_sub_field('type_lien');
+		$target = '';
 
-		if( $fluxi_content_lien ):
-
-			if( get_sub_field('afficher_bouton') == 1 ):
-				$fluxi_content_lien = '<a class="c-button c-button--ghost l-jump" href="'.get_sub_field('url_lien').'">'.get_sub_field('texte_lien').'</a>';
-			else:
-				$fluxi_content_lien = '<p><a href="'.get_sub_field('url_lien').'" class="c-link">'.get_sub_field('texte_lien').'</a></p>';
+		if( $fluxi_content_lien ):			
+			// Option externe
+			if ( $type_lien && in_array("externe", $type_lien) ):
+				$target = ' target="_blank"';				
+			endif;
+			// Bouton
+			if( $type_lien && in_array("bouton", $type_lien) ):
+				$fluxi_content_lien = '<a class="c-button c-button--ghost l-jump" href="'.get_sub_field('url_lien').'"'.$target.'>'.get_sub_field('texte_lien').'</a>';			
+			else:	
+				// Basic
+				$fluxi_content_lien = '<p><a href="'.get_sub_field('url_lien').'" class="c-link"'.$target.'>'.get_sub_field('texte_lien').'</a></p>';				
 			endif;
 
 			return $fluxi_content_lien;
