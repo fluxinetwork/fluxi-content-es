77630a874ea286fabdd29cc58487eadea6559d0d
Yann <rollandyann@gmail.com>
2020-09-23 15:38:16 +0200

    Sommaire

--- core/elements/get-bloc-titre.php
+++ core/elements/get-bloc-titre.php
@@ -17,10 +17,18 @@
 			
 		$taille_titre = get_sub_field('taille_titre');
 		$texte_titre = get_sub_field('texte_titre');		
+		$ajouter_sommaire = get_sub_field('ajouter_sommaire');
 
-		if( !empty($texte_titre) ):		
+		if( !empty($texte_titre) ):	
+
+			$data_titre_court = '';
+		    if( $ajouter_sommaire && get_sub_field('titre_court') ):
+		    	$data_titre_court = ' data-sommaire="'.get_sub_field('titre_court').'"';
+		    elseif($ajouter_sommaire && get_sub_field('titre_court') == ''):
+		    	$data_titre_court = ' data-sommaire="'.$texte_titre.'"';
+		    endif;
 			
-			$fluxi_content_titre = '<h'.$taille_titre.'>'.$texte_titre.'</h'.$taille_titre.'>';
+			$fluxi_content_titre = '<h'.$taille_titre.' '.$data_titre_court.'>'.$texte_titre.'</h'.$taille_titre.'>';
 
 			return $fluxi_content_titre;
 
