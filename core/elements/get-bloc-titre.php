24ea40dc2a16fab79f8333aeb60000f3dc8a1d35
Rolland Yann <rollandyann@gmail.com>
2020-10-26 12:10:45 +0100

    Sommaire

--- core/elements/get-bloc-titre.php
+++ core/elements/get-bloc-titre.php
@@ -18,16 +18,24 @@
 		$taille_titre = get_sub_field('taille_titre');
 		$texte_titre = get_sub_field('texte_titre');		
 
-		if( !empty($texte_titre) ):		
-			
-			$fluxi_content_titre = '<h'.$taille_titre.'>'.$texte_titre.'</h'.$taille_titre.'>';
+		if( !empty($texte_titre) ):	
+
+			if(get_sub_field('ajouter_sommaire')):
+				if(get_sub_field('titre_sommaire')):
+					$short_title = get_sub_field('titre_sommaire');
+				else:
+					$short_title = cut_string( $texte_titre, 40);
+				endif;
+
+				$fluxi_content_titre = '<h'.$taille_titre.' class="js-sommaire" data-title="'.$short_title.'">'.$texte_titre.'</h'.$taille_titre.'>';
+			else:
+				$fluxi_content_titre = '<h'.$taille_titre.'>'.$texte_titre.'</h'.$taille_titre.'>';
+			endif;
 
 			return $fluxi_content_titre;
 
 		endif;	
 		
 	}
-
-
 
 ?>
